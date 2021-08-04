5db5f69484d704b453f5ed54c64f4bf7dcea18a1
dnaram <markoandreis07@gmail.com>
2021-08-04 18:50:40 +0200

    Solve week-2 part 3

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -14,6 +14,7 @@ public class PizzaApp {
         PizzaDeliveryService pds = context.getBean("pizzaDeliveryService", PizzaDeliveryService.class);
         System.out.println(pds.orderPizza(new Carbonara()));
         context.close();
+
     }
 
 }
